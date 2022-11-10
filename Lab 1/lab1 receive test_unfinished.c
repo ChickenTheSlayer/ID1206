81e2d6b47bb12d035374133aab484b8aa2ff744b
ChickenTheSlayer <87721901+ChickenTheSlayer@users.noreply.github.com>
2022-11-10 23:34:02 +0100

    Add files via upload

---
+++ Lab 1/lab1 receive test_unfinished.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <mqueue.h>
+    const int MAX_SIZE = 1024;
+    const int MAX_NUM_MSG = 10;
+    const char *my_mq = "/mymq";
+
+int main(int argc, char const *argv[])
+{
+
+    struct mq_attr attr;
+    attr.mq_maxmsg = MAX_NUM_MSG;
+    attr.mq_msgsize = MAX_SIZE;
+
+
+    int pid = fork();
+
+    if (pid < 0)
+    {
+        perror("fork error \n");
+        exit(1);
+    }
+    if (pid == 0) // Child test
+    {
+    char buf[MAX_SIZE];
+
+    FILE *file;
+    file = fopen("test.txt", "r");
+    if (file == NULL){
+    perror("No file \n");
+    }
+
+
+    fgets(buf, MAX_SIZE, file);
+    mqd_t mq = mq_open(my_mq, O_CREAT | O_WRONLY, (S_IRUSR | S_IWUSR), &attr);
+            
+    mq_send(mq, buf, strlen(buf), 0);
+    mq_close(mq);
+
+    }
+    else if (pid > 0){
+    wait(NULL);
+    char buf[MAX_SIZE];
+    mqd_t mq = mq_open(my_mq, O_RDONLY);
+
+    mq_receive(mq, buf, MAX_SIZE, NULL);
+    mq_close(mq);
+    mq_unlink(my_mq);
+
+    char *token = strtok(buf, " ");
+    int i = 0;
+    while (token != NULL)
+    {
+         i++;
+        token = strtok(NULL, " ");
+    }
+
+    // count words in rcv
+    printf("%d\n", i);
+
+    }
+ 
+
+}
