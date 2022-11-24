786751f267f0ebfdc812a5dd04ea5beb144c5a6a
ChickenTheSlayer <87721901+ChickenTheSlayer@users.noreply.github.com>
2022-11-24 16:32:13 +0100

    Update Lab2.c

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -1,26 +1,115 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 
 int buffer = 0;
-pthread_mutex_t lock;
+pthread_mutex_t mutex;
 
-void* thread_func(void* arg) {
-	void* count = 0;
-	while (pthread_mutex_lock(&lock) || buffer < 15) {
-		printf("TID: %d, PID: %d, Buffer: %d\n", pthread_self(), getpid(), buffer);
-		pthread_mutex_unlock(&lock);
-		count++
-	}
+void *thread_func() {
+{
+    void *counter;
+    counter = 0;
+    while (pthread_mutex_lock(&mutex) || buffer < 15)
+    {
+     
+        printf("TID: %lu, PID: %d, Buffer: %d\n", pthread_self(), getpid(), buffer);
+        pthread_mutex_unlock(&mutex);
 
-int main() {
+        buffer++;
+        counter++;
+        sleep(0.1);
 
-	//Array of threads to be joined upon
-	pthread_t arr[3];
-	pthread_mutex_init(&lock, NULL);
-	for (int i = 0; i < 3; pthread_create(&threads[i++], NULL, thread_func, NULL));
+    }
+    pthread_mutex_unlock(&mutex);
+
+
+    return (void *)counter;
+}
+}
+int main()
+{
+    pthread_t thread1, thread2, thread3;
+
+    int thread1_count;
+    int thread2_count;
+    int thread3_count;
+
+    pthread_mutex_init(&mutex, NULL);
+
+    pthread_create(&thread1, NULL, &thread_func, NULL);
+    pthread_create(&thread2, NULL, &thread_func, NULL);
+    pthread_create(&thread3, NULL, &thread_func, NULL) ;
+
+    pthread_join(thread1, (void **)&thread1_count);
+    pthread_join(thread2, (void **)&thread2_count);
+    pthread_join(thread3, (void **)&thread3_count);
+
+    printf("TID %lu worked on the buffer %d times\n", thread1, thread1_count);
+    printf("TID %lu worked on the buffer %d times\n", thread2, thread2_count);
+    printf("TID %lu worked on the buffer %d times\n", thread3, thread3_count);
+
+    printf("Total buffer accesses: %d\n", buffer);
+    pthread_exit(NULL);
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int buffer = 0;
+pthread_mutex_t mutex;
 
 
 
+void *thread_func() {
+{
+    void *counter;
+    counter= 0;
+
+
+    for (counter; pthread_mutex_lock(&mutex) || buffer < 15; counter++, pthread_mutex_unlock(&mutex))
+    {
+        printf("TID: %lu, PID: %d, Buffer: %d\n", pthread_self(), getpid(), buffer);
+        pthread_mutex_unlock(&mutex);
+        buffer++;
+        sleep(0.1);
+
+    }
+    pthread_mutex_unlock(&mutex);
+    return (void *)counter;
+}
+}
+int main()
+{
+    pthread_t thread1, thread2, thread3;
+
+    int thread1_count;
+    int thread2_count;
+    int thread3_count;
+
+    pthread_mutex_init(&mutex, NULL);
+
+    pthread_create(&thread1, NULL, &thread_func, NULL);
+    pthread_create(&thread2, NULL, &thread_func, NULL);
+    pthread_create(&thread3, NULL, &thread_func, NULL);
+
+    pthread_join(thread1, (void **)&thread1_count);
+    pthread_join(thread2, (void **)&thread2_count);
+    pthread_join(thread3, (void **)&thread3_count);
+
+    printf("TID %lu worked on the buffer %d times\n", thread1, thread1_count);
+    printf("TID %lu worked on the buffer %d times\n", thread2, thread2_count);
+    printf("TID %lu worked on the buffer %d times\n", thread3, thread3_count);
+
+    printf("Total buffer accesses: %d\n", buffer);
+    pthread_exit(NULL);
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
 }
