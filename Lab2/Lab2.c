24265caa2449d729bea6233b73e2cd2fbe06a027
ChickenTheSlayer <87721901+ChickenTheSlayer@users.noreply.github.com>
2022-11-24 16:46:57 +0100

    Update Lab2.c

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -1,34 +1,34 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
 
 int buffer = 0;
 pthread_mutex_t lock;
 
-void* thread_func() {
-    {
-        void* counter;
-        counter = 0;
-        for (counter; pthread_mutex_lock(&lock) || buffer < 15; counter++)
-        {
-            printf("TID: %lu, PID: %d, Buffer: %d\n", pthread_self(), getpid(), buffer);
-            pthread_mutex_unlock(&lock);
-            buffer++;
-            sleep(0.1);
 
-        }
+
+void *thread_func() {
+{
+    void *counter;
+    counter= 0;
+    for (counter; pthread_mutex_lock(&lock) || buffer < 15; counter++)
+    {
+        buffer++;
+        printf("TID: %lu, PID: %d, Buffer: %d\n", pthread_self(), getpid(), buffer);
         pthread_mutex_unlock(&lock);
-        return (void*)counter;
+        sleep(0.5);
+
     }
+    pthread_mutex_unlock(&lock);
+    return (void *)counter;
+}
 }
 int main()
 {
     pthread_t thread1, thread2, thread3;
 
-    int thread1_count;
-    int thread2_count;
-    int thread3_count;
+    int thread1_count,thread2_count,thread3_count;
 
     pthread_mutex_init(&lock, NULL);
 
@@ -36,9 +36,9 @@ int main()
     pthread_create(&thread2, NULL, &thread_func, NULL);
     pthread_create(&thread3, NULL, &thread_func, NULL);
 
-    pthread_join(thread1, (void**)&thread1_count);
-    pthread_join(thread2, (void**)&thread2_count);
-    pthread_join(thread3, (void**)&thread3_count);
+    pthread_join(thread1, (void **)&thread1_count);
+    pthread_join(thread2, (void **)&thread2_count);
+    pthread_join(thread3, (void **)&thread3_count);
 
     printf("TID %lu worked on the buffer %d times\n", thread1, thread1_count);
     printf("TID %lu worked on the buffer %d times\n", thread2, thread2_count);
