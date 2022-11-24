85411df74bd37ee02fe1214d4967e6e8ec07b4d8
ChickenTheSlayer <87721901+ChickenTheSlayer@users.noreply.github.com>
2022-11-24 14:27:25 +0100

    Update Lab2.c

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
+int buffer = 0;
+pthread_mutex_t lock;
+
+void* thread_func(void* arg) {
+	void* count = 0;
+	while (pthread_mutex_lock(&lock) || buffer < 15) {
+		printf("TID: %d, PID: %d, Buffer: %d\n", pthread_self(), getpid(), buffer);
+		pthread_mutex_unlock(&lock);
+		count++
+	}
+
+int main() {
+
+	//Array of threads to be joined upon
+	pthread_t arr[3];
+	pthread_mutex_init(&lock, NULL);
+	for (int i = 0; i < 3; pthread_create(&threads[i++], NULL, thread_func, NULL));
+
+
+
+}
